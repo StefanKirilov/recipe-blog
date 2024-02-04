15e1d79ba7acff4fae647f7a45663fa51bcb1826
StefanKirilov <stefank5248@gmail.com>
2024-02-04 16:01:55 +0200

    new

--- src/app/user/min-count.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MinCountDirective } from './min-count.directive';
-
-describe('MinCountDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MinCountDirective();
-    expect(directive).toBeTruthy();
-  });
-});
